85a85ab178c3e93607cfe83087695c21b16821ae
Yinson Tso <yinson.tso@gmail.com>
2023-05-01 16:06:17 -0400

    oxford

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
 def oxford_comma(items):
+    if len(items) == 1:
+        return ''.join(items)
+    elif len(items) == 2:
+        return ' and '.join(items)
+    else:
+        l = items[:-1]
+        l.append("and " + items[-1]) #add a "and " to last element 
+        return ", ".join(l)
+
+
+    # ', '.join(items)
     return None
